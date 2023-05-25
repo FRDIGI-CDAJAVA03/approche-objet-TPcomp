1ab8b5e630ece956601c53b650a1f04ee8bd5b27
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-05-25 10:59:54 +0200

    Exercice cercle OK

--- src/fr/diginamic/exerciceCercle/TestCercle.java
+++ src/fr/diginamic/exerciceCercle/TestCercle.java
@@ -3,7 +3,19 @@ package fr.diginamic.exerciceCercle;
 import mochizukiTools.Utils;
 
 public class TestCercle {
+
     public static void run() {
         Utils.msgTitle("EXERCICE 2 : CERCLE");
+        Utils.msgInfo("Instanciation de 2 cercles:");
+
+        Cercle cercle1 = new Cercle(5.52);
+        double rayonCercle1 = cercle1.getRayon();
+        String strMsg = String.format("Cercle1 => rayon: %.4f  périmètre: %.4f     surface: %.4f",rayonCercle1,cercle1.setPerimetre(rayonCercle1),cercle1.setSurface(rayonCercle1));
+        Utils.msgResult(strMsg);
+
+        Cercle cercle2 = new Cercle(18.456);
+        double rayonCercle2 = cercle2.getRayon();
+        strMsg = String.format("Cercle2 => rayon: %.4f  périmètre: %.4f     surface: %.4f",rayonCercle2,cercle2.setPerimetre(rayonCercle2),cercle1.setSurface(rayonCercle2));
+        Utils.msgResult(strMsg);
     }
 }
